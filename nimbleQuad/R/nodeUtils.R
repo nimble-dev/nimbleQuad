be245a9dd22706cccb39ff49a8a0a31d2910131b
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-03-25 16:03:36 -0700

    Add missing nodeUtils.R

---
+++ nimbleQuad/R/nodeUtils.R
@@ -0,0 +1,58 @@
+## TODO: these might be moved to `nimble`.
+
+makeNodeString <- function(nodes, model) {
+    if (!length(nodes))
+        return("")
+    elements <- model$expandNodeNames(nodes, returnScalarComponents = TRUE)
+    vars <- sapply(strsplit(elements, "[", fixed = TRUE), `[[`, 1)
+    nodesCount <- table(vars)
+    start <- names(nodesCount)
+    end <- c(rep(", ", length(start) - 1), "")
+    end[nodesCount > 1] <- paste0(" (", nodesCount[nodesCount > 1], " elements)",
+        end[nodesCount > 1])
+    return(paste0(c(rbind(start, end)), collapse = ""))
+}
+
+splitLatents <- function(model, paramNodes, latentNodes, calcNodes, calcNodesOther,
+    control = list()) {
+    stochNodes <- model$getNodeNames(stochOnly = TRUE, includeData = FALSE)
+    discreteStochNodes <- model$isDiscrete(stochNodes)
+    if (any(discreteStochNodes))
+        stop("splitLatents: found discrete non-data stochastic nodes in processing nodes for quadrature-based posterior approximation: ",
+            paste0(stochNodes[discreteStochNodes], collapse = ", "), ". Discrete non-data stochastic nodes cannot be handled by the posterior approximation algorithm.")
+    split <- extractControlElement(control, "split", TRUE)
+    check <- extractControlElement(control, "check", TRUE)
+    margNodes <- setupMargNodes(model = model, paramNodes = paramNodes, randomEffectsNodes = latentNodes,
+        calcNodes = calcNodes, calcNodesOther = calcNodesOther, split = split, check = check)
+    if (missing(paramNodes) && missing(latentNodes)) {
+        if (!missing(calcNodes) || !missing(calcNodesOther))
+            messageIfVerbose("   [Note] Ignoring provide `calcNodes` and `calcNodesOther` because `paramNodes` and `latentNodes` not provided and are being determinted automatically.")
+        paramNodes <- margNodes$paramNodes
+        latentNodes <- margNodes$randomEffectsNodes
+        deps <- model$getDependencies(latentNodes, includeData = FALSE, self = FALSE)
+        ## By default, we treat "siblings" of latent nodes as latents.
+        ## This attempts to have fixed effects in latents,
+        ## along with random effects.
+        newLatents <- model$getParents(deps)
+        paramNodes <- setdiff(paramNodes, newLatents)
+        margNodes <- setupMargNodes(model = model, paramNodes = paramNodes,
+            randomEffectsNodes = c(latentNodes, newLatents), split = split, check = check)
+    }
+
+    messageIfVerbose("  [Note] Posterior approximation is using the following node sets:\n",
+                     "         parameter nodes: ", makeNodeString(paramNodes, model), "\n",
+                     "         latent nodes: ", makeNodeString(latentNodes, model))
+    if (length(paramNodes) > 20)
+        messageIfVerbose("  [Warning] There is a large number of parameter node elements. Computation may be slow.")
+    return(margNodes)
+}
+
+nodesToIndices <- function(node, approx) {
+    Rapprox <- ifelse(is(approx, "NestedApprox"), approx, approx$Robject)
+    if (!node %in% Rapprox$paramNodesComponents)
+        stop("`", node, "` not found in model")
+    index <- paramNodesIndices[which(node == paramNodesComponents)]
+    if (index == 0)
+        stop("`", node, "` is part of a multivariate node for which the parameter transformation is not one-to one. Use `sampleLatentNodes()` to sample from the approximate posterior for the parameters.")
+    return(index)
+}
