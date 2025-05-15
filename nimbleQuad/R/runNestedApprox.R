283c578f415b0968ec3dad016f0734c0eed9e0a2
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-05-15 10:51:52 -0700

    Add arg to turn off default 1d marg improvement.

--- nimbleQuad/R/runNestedApprox.R
+++ nimbleQuad/R/runNestedApprox.R
@@ -127,7 +127,8 @@ approxSummary <- R6Class("approxSummary",
 ## Main user-facing function for running a nested approximation and getting a
 ## results summary.  
 runNestedApprox <- function(approx, quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975),
-                            originalScale = TRUE, nSamplesLatents = 0, nSamplesParams = 0) {
+                            originalScale = TRUE, improve1d = TRUE,
+                            nSamplesLatents = 0, nSamplesParams = 0) {
     if(is(approx, "NestedApprox")) 
         Rapprox <- approx else Rapprox <- approx$Robject
 
@@ -179,7 +180,7 @@ runNestedApprox <- function(approx, quantiles = c(0.025, 0.25, 0.5, 0.75, 0.975)
 
     ## With only one parameter, computations should not be so slow, so go ahead
     ## and use better marginal and logLik estimates.
-    if (nParamTrans == 1) {
+    if (nParamTrans == 1 && improve1d) {
         improveMarginals(summary, ifelse(originalScale, Rapprox$paramNodesComponents[1], 1))
         summary$marginalLogLik_improved <- approx$calcMarginalLogLikQuad()
     }
