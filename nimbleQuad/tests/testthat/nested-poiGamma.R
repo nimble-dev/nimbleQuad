1922408db9e9df201170eaa834bb60edd1c25c54
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-06-09 10:04:46 -0700

    Update some examples in light of fixes to latent transformation.

--- nimbleQuad/tests/testthat/nested-poiGamma.R
+++ nimbleQuad/tests/testthat/nested-poiGamma.R
@@ -23,6 +23,17 @@ y <- matrix(rpois(n*J, mns), ncol = J)
 m <- nimbleModel(code, data = list(y = y), constants = list(n=n, J=J),
                  inits = list(mu = rep(0,J), a=1,b=1), buildDerivs = TRUE)
 
+## Check that Laplace works (i.e., with the RE transformation)
+cm <- compileNimble(m)
+mLaplace <- buildLaplace(model = m)
+cmLaplace <- compileNimble(mLaplace, project = m)
+result <- runLaplace(cmLaplace, jointCovariance = TRUE)
+result <- runLaplace(cmLaplace, jointCovariance = TRUE, originalScale = FALSE)
+result$summary
+
+
+## Question: are we transforming latents when using nested approx?
+
 approx <- buildNestedApprox(m, latentNodes = c('mu'), hyperParamNodes = c('a','b'))
 cm <- compileNimble(m)
 capprox <- compileNimble(approx, project = m)
