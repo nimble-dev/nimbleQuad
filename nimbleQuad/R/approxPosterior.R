85711ede6980090f7b998657af40ba8e802464e3
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-06-13 11:35:56 -0700

    Fix spacing in a note.

--- nimbleQuad/R/approxPosterior.R
+++ nimbleQuad/R/approxPosterior.R
@@ -440,7 +440,7 @@ buildNestedApprox <- nimbleFunction(
         ## Probably not particularly accurate for CCD.
         calcMarginalLogLikQuad = function() {
             if (I_GRID == I_CCD)
-                print("  [Note]: Estimating marginal log-likelihood based on CCD grid.\n           Estimation based on an AGHQ grid may be more accurate (but more computationally expensive).")
+                print("  [Note]: Estimating marginal log-likelihood based on CCD grid.\n          Estimation based on an AGHQ grid may be more accurate (but more computationally expensive).")
             if(!hyperGridCached[I_GRID])
                 calcHyperGrid()
             returnType(double())
