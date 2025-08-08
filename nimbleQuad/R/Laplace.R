0706cfce9340cf2e9cab3d6bfb2bed5d8b731855
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-08-07 17:21:42 -0700

    Fix syntax error in merge conflict fix.

--- nimbleQuad/R/Laplace.R
+++ nimbleQuad/R/Laplace.R
@@ -1901,7 +1901,7 @@ buildAGHQ <- nimbleFunction(
         if(nQuad_ == 1)
             msg <- "Laplace" else msg <- "AGHQ (extended Laplace)"
         if(length(reSets) > 1) {
-            messageIfVerbose("Building individual ", msg, " approximations (one dot for each): ", appendLF = FAL
+            messageIfVerbose("Building individual ", msg, " approximations (one dot for each): ", appendLF = FALSE)
         } else {
           messageIfVerbose("Building ", msg, " approximation.")
         }
