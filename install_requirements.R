44361bb428de5e34a463d0095b1164697b4c5db4
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-08-08 16:45:23 -0700

    Update testing reqs to use nimble devel branch.

--- install_requirements.R
+++ install_requirements.R
@@ -24,6 +24,6 @@ install.packages('lme4', type = 'source')
 ## later, remove below:
 install.packages('devtools')
 library(devtools)
-devtools::install_github('nimble-dev/nimble', ref = 'remove-quad', subdir = 'packages/nimble')
+devtools::install_github('nimble-dev/nimble', ref = 'devel', subdir = 'packages/nimble')
 ## remove until here
 
