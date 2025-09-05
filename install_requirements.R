a06f2cfcc274991b6c0dd5c505f3090634415d3b
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-09-05 11:22:25 -0700

    Update how nimble installed in CI to try to access test_utils.R.

--- install_requirements.R
+++ install_requirements.R
@@ -24,6 +24,6 @@ install.packages('lme4', type = 'source')
 ## later, remove below:
 install.packages('devtools')
 library(devtools)
-devtools::install_github('nimble-dev/nimble', ref = 'devel', subdir = 'packages/nimble')
+devtools::install_github('nimble-dev/nimble', ref = 'devel', subdir = 'packages/nimble', INSTALL_opts = c("--install-tests"))
 ## remove until here
 
