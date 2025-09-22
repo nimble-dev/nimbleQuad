af2939d1daf501f8f9e019754a90d41d777d27fc
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-09-22 12:52:07 -0700

    Tweak testing to try to deal with PITA CI errors.

--- nimbleQuad/tests/testthat/test-laplace.R
+++ nimbleQuad/tests/testthat/test-laplace.R
@@ -599,11 +599,11 @@ test_that("Laplace simplest 2x1D works, with multiple data for each", {
   # Hessian of marginal loglik wrt mu: -0.04511278 (numerical, have not got AD work)
   muhat <- mean(y)/(0.8*0.5)
   ahat <- c((9*0.8*sum(y[1,]) + 4*0.5*muhat)/(4+9*0.8^2*3), (9*0.8*sum(y[2,]) + 4*0.5*muhat)/(4+9*0.8^2*3))
-  summ <- cmLaplace$summary(opt, originalScale = TRUE, randomEffectsStdError = TRUE, jointCovariance = TRUE)
-  expect_equal(summ$randomEffects$estimate, ahat, tol = 1e-6)
+  summ1 <- cmLaplace$summary(opt, originalScale = TRUE, randomEffectsStdError = TRUE, jointCovariance = TRUE)
+  expect_equal(summ1$randomEffects$estimate, ahat, tol = 1e-6)
   # Covariance matrix
-  vcov <- diag(c(0, rep(1/(3*0.8^2/4 + 1/9), 2))) + matrix(c(1, rep(0.09398496, 2)), ncol = 1) %*% (1/0.04511278) %*% t(matrix(c(1, rep(0.09398496, 2)), ncol = 1))
-  expect_equal(vcov, summ$vcov, tol = 1e-7)
+  vcov2d <- diag(c(0, rep(1/(3*0.8^2/4 + 1/9), 2))) + matrix(c(1, rep(0.09398496, 2)), ncol = 1) %*% (1/0.04511278) %*% t(matrix(c(1, rep(0.09398496, 2)), ncol = 1))
+  expect_equal(vcov2d, summ1$vcov, tol = 1e-7)
   ## Check covariance matrix for params only
   tryResult <- try({
       summ2 <- cmLaplace$summary(opt, originalScale = TRUE, randomEffectsStdError = TRUE, jointCovariance = FALSE)
