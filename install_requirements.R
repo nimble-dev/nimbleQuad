5590ab547e80f42ba663c33b396e6742568ff706
Christopher Paciorek <paciorek@stat.berkeley.edu>
2025-11-13 12:34:14 -0800

    Use nimble no-laplace version for testing.

--- install_requirements.R
+++ install_requirements.R
@@ -24,6 +24,6 @@ install.packages('lme4', type = 'source')
 ## later, remove below:
 install.packages('devtools')
 library(devtools)
-devtools::install_github('nimble-dev/nimble', ref = 'devel', subdir = 'packages/nimble', INSTALL_opts = c("--install-tests"))
+devtools::install_github('nimble-dev/nimble', ref = 'no-laplace', subdir = 'packages/nimble', INSTALL_opts = c("--install-tests"))
 ## remove until here
 
